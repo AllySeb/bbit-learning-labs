007da5920f1fd79ae8781cc991659cec8fe7adc3
Ally Seba <aseba@usc.edu>
2024-03-01 15:53:27 -0800

    consume.py complete

--- Tech-Lab-On-Campus/Topic-Exchange/consume.py
+++ Tech-Lab-On-Campus/Topic-Exchange/consume.py
@@ -23,11 +23,11 @@ def main(sector: str, queueName: str) -> None:
     #
     #                       WRITE CODE HERE!!!
     #
-    
+    self.bindingKey = "*.TECHLAB.*"
+
     consumer = mqConsumer(binding_key=bindingKey,exchange_name="Tech Lab Topic Exchange",queue_name=queueName)    
     consumer.startConsuming()
     
-
 
 if __name__ == "__main__":
 
@@ -35,5 +35,7 @@ if __name__ == "__main__":
     #
     #                       WRITE CODE HERE!!!
     #
+    self.sector = sys.argv[0]
+    self.queueName = sys.argv[1]
 
     sys.exit(main(sector,queue))
